2510a1889659141b1aa2bcaf5925edc45b7faa67
AbdulKarim Mandaviya <abdulmandaviya@gmail.com>
2022-06-16 17:56:40 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App () {
+
+const [newItem, setNewItem] = useState("")
+const [items, setItems] = useState([])
+
+
+ function  addItem() {
+       // console.log(newItem)
+
+       if(!newItem) {
+        alert ("Enter an item")
+        return
+       }
+
+       const item = {
+        id: Math.floor(Math.random() * 1000),
+        value: newItem
+       }
+         setItems(oldList => [...oldList, item])
+         setNewItem("")
+         //console.log(items)
+        }
+
+function deleteItems (id) {
+    const newArray = items.filter(item => item.id !== id)
+    setItems(newArray)
 }
 
-export default App;
+    return (
+        <div className="App">
+            <h1>To-do List</h1>
+            <input type="text" placeholder="Add an item"
+            value={newItem}
+            onChange = {e => setNewItem(e.target.value)}
+            />
+            <button onClick={() => addItem()}>Add</button>
+
+            <ul>
+                {items.map(item => {
+                    return (
+                        <li key = {item.id}>{item.value} 
+                        <button onClick={() => deleteItems(item.id)}>Delete</button>
+                         </li>
+                      
+                    )
+                })}
+            </ul>
+        </div>
+    )
+}
+export default App
